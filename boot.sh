f29994fb4195f83540bf92fcc7e0cf149b28233c
John Montgomery <johnbmontgomery@gmail.com>
2020-04-26 17:43:24 -0400

    Testing that docker works on a remote machine

---
+++ boot.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# this script is used to boot a Docker container
+source venv/bin/activate
+while true; do
+    flask db upgrade
+    if [[ "$?" == "0" ]]; then
+        break
+    fi
+    echo Deploy command failed, retrying in 5 secs...
+    sleep 5
+done
+
+flask run
