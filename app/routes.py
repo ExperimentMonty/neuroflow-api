22318927ff2c49cd10456dfc8c6c211806aaaa92
John Montgomery <johnbmontgomery@gmail.com>
2020-04-26 10:00:49 -0400

    Add the ability for users to login
    Currently, the only login process is basic auth, there's no restrictions
    on what you can put into your username or password, though I at least
    made it so you can't overwrite someone else's username. You currently
    can't update your password either.

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,34 @@
-from flask import request, jsonify
+from flask import request, jsonify, g, abort
 from sqlalchemy import desc
 
-from app import app, db
-from app.models import Mood
+from app import app, db, auth
+from app.models import Mood, User
 
 
-@app.route('/mood', methods=['POST'])
+@auth.verify_password
+def verify_password(username, password):
+    user =  User.query.filter_by(username=username).first()
+    if not user or not user.check_password(password):
+        return False
+    g.user = user
+    return True
+
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    username = request.json['username']
+    password = request.json['password']
+    if User.query.filter_by(username=username).first() is not None:
+        abort(400)
+    user = User(username=username)
+    user.set_password(password)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({'username': username})
+
+
+@app.route('/moods', methods=['POST'])
+@auth.login_required
 def set_mood():
     mood = Mood(value=request.json['mood'])
     db.session.add(mood)
@@ -13,7 +36,8 @@ def set_mood():
     return jsonify(mood.as_dict())
 
 
-@app.route('/mood', methods=['GET'])
+@app.route('/moods', methods=['GET'])
+@auth.login_required
 def get_mood():
     moods = Mood.query.order_by(desc('id')).all()
     return jsonify([mood.as_dict() for mood in moods])
